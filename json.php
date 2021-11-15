201ee3ad84a22c6425788f46b50d338ec8470ae6
himy3210 <55615434+himy3210@users.noreply.github.com>
2021-11-15 11:06:00 +0900

    Update json.php

--- json.php
+++ json.php
@@ -16,19 +16,3 @@ else if($type == "POST") {
     echo(json_encode(array("mode" => $_REQUEST['mode'], "user_no" => $user_no, "contents" => $contents))); 
     
 ?>
-
-
-<?php
- 
-$chatlist_jsonlist = json_decode($_POST['chatlist'],true);     //POST로 받은 값을 json형식으로 decode
- 
-for($i = 0 ; $i < count($chatlist_jsonlist) ; $i++){
-
-  //JSONArray에서 [$i] 번째 행의 JSONObject [' '] 항목의 값을 가져옴
-  $member_nm = "'".$chatlist_jsonlist[$i]['member_nm']."'";
-  $chatmsg = "'".$chatlist_jsonlist[$i]['chatmsg']."'";
-  $msg_timestamp = "'".$chatlist_jsonlist[$i]['timestamp']."'";
- 
-  //테스트 출력
-  echo $member_nm."/".$chatmsg."/".$msg_timestamp;
-}
